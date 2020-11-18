78d5fb9a4f7818376df921e9b147e7720012b5b3
Dipu <dpumail.in@gmail.com>
2020-11-18 22:10:10 +0600

    pessanger added

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight
+from .models import Flight, Passenger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -8,6 +10,26 @@ def index(request):
 
 
 def flight(request, flight_id):
-    flight = Flight.objects.filter(pk=flight_id)
+    flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": passengers,
+        "non_passengers": non_passengers
+    })
 
-    return render(request, 'flights/flight.html', {'flight': flight})
+
+def book(request, flight_id):
+
+    if request.method == "POST":
+
+        flight = Flight.objects.get(pk=flight_id)
+
+        passenger_id = int(request.POST["passenger"])
+
+        passenger = Passenger.objects.get(pk=passenger_id)
+
+        passenger.flights.add(flight)
+
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
